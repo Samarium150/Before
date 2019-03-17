dc4db9aee092894a9ccbc4709d5849abc4d3e5b2
chanja60 <jace.chan@mail.utoronto.ca>
2019-03-17 02:42:00 -0400

    Put every scingle button the the board, but they are not alighed correctly

--- src/ca/utoronto/utm/chess/ChessBoardPanel.java
+++ src/ca/utoronto/utm/chess/ChessBoardPanel.java
@@ -12,18 +12,47 @@ public class ChessBoardPanel extends StackPane{
 	protected View view;
 
 	
-	Button[][] button = new Button[8][8];
+	
 	
 	public ChessBoardPanel(View view) {
+		Button[][] button = new Button[8][8];
+		//Button[] button = new Button[8];
 		this.view = view;
 		this.canvas = new Canvas(800, 600);
 		this.getChildren().add(this.canvas);
 		this.setStyle("-fx-background-color: teal");
 
+
+		for(int i = 0; i < 8; i++) {
+			for(int j = 0; j < 8; j++) {
+				button[i][j] = new Button(i + ":" + j);
+				this.getChildren().add(button[i][j]);
+			}
+			
+		}
+	
+/*		
+		Button button1 = new Button("Start");
+		this.getChildren().add(button1);
+	//	this.setAlignment(button, Pos.CENTER);
+
+		Button button2 = new Button("Settings");
+		this.getChildren().add(button2);
+
+		
+		Button button3 = new Button("asdfaaa");
+		this.getChildren().add(button3);	
 		
 
 		
 
+		Button[] buttons = new Button[3];
+		
+		for(int i = 0; i<3;i++) {
+			buttons[i] = new Button("asd");
+					
+		}
+*/	
 	}
 	
 
