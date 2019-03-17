7e8e7b4b8a46858c51eed188f15df635d93d5963
chanja60 <jace.chan@mail.utoronto.ca>
2019-03-17 02:18:26 -0400

    Added buttons array for each square on the chess board

--- src/ca/utoronto/utm/chess/ChessBoardPanel.java
+++ src/ca/utoronto/utm/chess/ChessBoardPanel.java
@@ -1,5 +1,26 @@
 package ca.utoronto.utm.chess;
 
-public class ChessBoardPanel {
+import javafx.scene.control.Button;
+import javafx.geometry.Pos;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+
+public class ChessBoardPanel extends StackPane{
+	protected Canvas canvas;
+	protected View view;
+
+	
+	Button[][] button = new Button[8][8];
+	
+	public ChessBoardPanel(View view) {
+		this.view = view;
+		this.canvas = new Canvas(800, 600);
+		this.getChildren().add(this.canvas);
+		
+
+	}
+	
 
 }
