7a9f1d07b14e72f6fb0f1e1a82cafd45df16ac35
chanja60 <jace.chan@mail.utoronto.ca>
2019-03-18 19:56:42 -0400

    Added place for ip for multiplayer functions

--- src/ca/utoronto/utm/chess/MultiplayerPanel.java
+++ src/ca/utoronto/utm/chess/MultiplayerPanel.java
@@ -1,5 +1,9 @@
 package ca.utoronto.utm.chess;
 
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
 public class MultiplayerPanel extends StackPane {
@@ -8,7 +12,34 @@ public class MultiplayerPanel extends StackPane {
 	
 	public MultiplayerPanel(View view) {
 		this.view = view;
+		/*
+		TextField text1 = new TextField();
+		text1.setPrefColumnCount(20);
+		this.getChildren().add(text1);
 		
+		TextField text2 = new TextField();
+		text2.setPrefColumnCount(20);
+		this.getChildren().add(text2);
+		
+		*/
+		HBox pane = new HBox(5);
+		pane.setPadding(new Insets(10));
+
+	
+
+		TextField text1 = new TextField();
+		text1.setPrefColumnCount(15);
+		pane.getChildren().add(text1);
+		
+		TextField text2 = new TextField();
+		text2.setPrefColumnCount(15);
+		pane.getChildren().add(text2);
+		
+		Button connect = new Button("Connect");
+		pane.getChildren().add(connect);
+		
+		this.getChildren().add(pane);                
+
 	}
 
 }
