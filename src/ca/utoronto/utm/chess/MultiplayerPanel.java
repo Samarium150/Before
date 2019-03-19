54491dec4b3b0dd14f8e755d9040860d1ff77017
chanja60 <jace.chan@mail.utoronto.ca>
2019-03-18 20:11:23 -0400

    Two textfields are workable now

--- src/ca/utoronto/utm/chess/MultiplayerPanel.java
+++ src/ca/utoronto/utm/chess/MultiplayerPanel.java
@@ -3,6 +3,7 @@ package ca.utoronto.utm.chess;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
@@ -12,31 +13,24 @@ public class MultiplayerPanel extends StackPane {
 	
 	public MultiplayerPanel(View view) {
 		this.view = view;
-		/*
-		TextField text1 = new TextField();
-		text1.setPrefColumnCount(20);
-		this.getChildren().add(text1);
 		
-		TextField text2 = new TextField();
-		text2.setPrefColumnCount(20);
-		this.getChildren().add(text2);
-		
-		*/
 		HBox pane = new HBox(5);
 		pane.setPadding(new Insets(10));
 
 	
-
 		TextField text1 = new TextField();
 		text1.setPrefColumnCount(15);
+		text1.setTooltip(new Tooltip("IP Address"));
 		pane.getChildren().add(text1);
 		
 		TextField text2 = new TextField();
 		text2.setPrefColumnCount(15);
+		text2.setTooltip(new Tooltip("Port Number"));
 		pane.getChildren().add(text2);
 		
-		Button connect = new Button("Connect");
-		pane.getChildren().add(connect);
+		Button connect_btn = new Button("Connect");
+		pane.getChildren().add(connect_btn);
+		connect_btn.setOnAction(new MultiplayerPanelEventHandler(this.view,text1,text2));
 		
 		this.getChildren().add(pane);                
 
--- src/ca/utoronto/utm/chess/MultiplayerPanelEventHandler.java
+++ src/ca/utoronto/utm/chess/MultiplayerPanelEventHandler.java
@@ -2,18 +2,25 @@ package ca.utoronto.utm.chess;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.TextField;
 
 public class MultiplayerPanelEventHandler implements EventHandler<ActionEvent> {
 	
 	
 	View view;  
-
-	public MultiplayerPanelEventHandler(View view) {
+	TextField text1;
+	TextField text2;
+	
+	public MultiplayerPanelEventHandler(View view, TextField text1,TextField text2) {
 		this.view = view;
+		this.text1 = text1;
+		this.text2 = text2;
 	}
 
 	@Override
 	public void handle(ActionEvent event) {
+		System.out.println(this.text1.getText());
+		System.out.println(this.text2.getText());
 		
 	}
 
