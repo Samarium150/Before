f8d8b4c65c1421b7c60e62cd7abd6a4c19e6fdc0
chanja60 <jace.chan@mail.utoronto.ca>
2019-03-18 21:14:14 -0400

    Added popup window with instructions

--- src/ca/utoronto/utm/chess/ViewEventHandler.java
+++ src/ca/utoronto/utm/chess/ViewEventHandler.java
@@ -2,6 +2,8 @@ package ca.utoronto.utm.chess;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
 
 public class ViewEventHandler implements EventHandler<ActionEvent>{
@@ -18,7 +20,16 @@ public class ViewEventHandler implements EventHandler<ActionEvent>{
 		String command = ((MenuItem) event.getSource()).getId();
 		
 		if (command.equals("Quit")) {
+			this.view.createNewGame();
 			this.view.changeToStart();
+		}else if(command.equals("New Game")){
+			this.view.createNewGame();
+		}else if(command.equals("Help")) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+	        alert.setTitle("Instructions");
+	        alert.setHeaderText("The Game of Chess");
+	        alert.setContentText("THIS IS HOW YOU PLAY");
+	        alert.showAndWait();
 		}
 		
 	}
