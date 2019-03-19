bfed3d3ab55e1ba70985973025c01ba73dcff1cd
chanja60 <jace.chan@mail.utoronto.ca>
2019-03-18 20:33:34 -0400

    Added connection instructions

--- src/ca/utoronto/utm/chess/MultiplayerPanel.java
+++ src/ca/utoronto/utm/chess/MultiplayerPanel.java
@@ -1,13 +1,16 @@
 package ca.utoronto.utm.chess;
 
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 
-public class MultiplayerPanel extends StackPane {
+public class MultiplayerPanel extends FlowPane {
 	
 	View view;
 	
@@ -16,6 +19,13 @@ public class MultiplayerPanel extends StackPane {
 		
 		HBox pane = new HBox(5);
 		pane.setPadding(new Insets(10));
+		
+		
+		
+		Button help = new Button("Help");
+		help.setMinWidth(15);
+		pane.getChildren().add(help);
+		help.setOnAction(new MultiplayerPanelEventHandler(this.view));
 
 	
 		TextField text1 = new TextField();
@@ -28,10 +38,16 @@ public class MultiplayerPanel extends StackPane {
 		text2.setTooltip(new Tooltip("Port Number"));
 		pane.getChildren().add(text2);
 		
-		Button connect_btn = new Button("Connect");
+		Button connect_btn = new Button("Go");
+		connect_btn.setMinWidth(15);
 		pane.getChildren().add(connect_btn);
 		connect_btn.setOnAction(new MultiplayerPanelEventHandler(this.view,text1,text2));
 		
+		/*
+		Label label = new Label("To connect please put the IP address into the first textbox and then the port into the second textbox");
+		pane.getChildren().add(label);
+		this.setAlignment(label, Pos.CENTER);
+		*/
 		this.getChildren().add(pane);                
 
 	}
--- src/ca/utoronto/utm/chess/MultiplayerPanelEventHandler.java
+++ src/ca/utoronto/utm/chess/MultiplayerPanelEventHandler.java
@@ -2,6 +2,9 @@ package ca.utoronto.utm.chess;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class MultiplayerPanelEventHandler implements EventHandler<ActionEvent> {
@@ -17,11 +20,26 @@ public class MultiplayerPanelEventHandler implements EventHandler<ActionEvent> {
 		this.text2 = text2;
 	}
 
+	public MultiplayerPanelEventHandler(View view2) {
+		this.view = view;
+	}
+
 	@Override
 	public void handle(ActionEvent event) {
-		System.out.println(this.text1.getText());
-		System.out.println(this.text2.getText());
 		
+		String button_name = ((Button) event.getSource()).getText();
+		
+		if(button_name.equals("Help")) {
+			Alert alert = new Alert(AlertType.INFORMATION);
+	        alert.setTitle("Help!");
+	        alert.setHeaderText("Connection instructions");
+	        alert.setContentText("To connect please type the IP address into the first box and "
+	        		+ "then the port number into the second box.");
+	        alert.showAndWait();
+		}else {
+			System.out.println(this.text1.getText());
+			System.out.println(this.text2.getText());
+		}
 	}
 
 }
