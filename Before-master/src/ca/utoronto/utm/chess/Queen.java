95111ed01b049bcba6a81f7e24f6d8ba7ca7b9ae
cooperhatfield <47254427+cooperhatfield@users.noreply.github.com>
2019-03-19 18:32:46 -0400

    Revert "Revert "Integrated the CPU Opponent algorithm with the rest of the program.""
    This reverts commit ff1758459e68fc77dd2c25f237bede84a71cd8e6.

---
+++ Before-master/src/ca/utoronto/utm/chess/Queen.java
@@ -0,0 +1,27 @@
+package ca.utoronto.utm.chess;
+
+import java.util.ArrayList;
+
+
+public class Queen extends PieceFactory{
+	
+	public Queen(BoardFactory board, int id) {
+		this.board = board;
+		this.id = id;
+	}
+
+	@Override
+	public ArrayList<Integer> possibleMoves() {
+		ArrayList<Integer> moves =  new ArrayList<Integer>();
+		bishopOverallMovement(moves);
+		rockOverallMovement(moves);
+		return movesFilter(moves);
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return "Q";
+	}
+	
+}
